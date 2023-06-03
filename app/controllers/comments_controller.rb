261dd0873074327d73d0423221838b297fee4fdc
masahito@19770526 <masahito.shimada@enishi-advisory.com>
2023-06-03 11:16:07 +0900

    Commentsの重複の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,6 @@
 class CommentsController < ApplicationController
-  
   def create
     @comment = Comment.new(comment_params)
-    Comment.create(comment_params)
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
@@ -11,9 +9,9 @@ class CommentsController < ApplicationController
       render "prototypes/show"
       end
     end
-      
 
   private
+
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
